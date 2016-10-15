f98b1f9141d45ba763e978ef33eb5eac58215361
aneesahmadweb <aneesahmadweb@gmail.com>
2016-10-16 00:40:16 +0500

    commit

--- app/code/community/AW/Blog/controllers/MyController.php
+++ app/code/community/AW/Blog/controllers/MyController.php
@@ -5,9 +5,7 @@ class AW_Blog_MyController extends Mage_Core_Controller_Front_Action
 
   public function preDispatch(){
 
-      $this->loadLayout();
-      echo $this->getLayout()->getBlock('root')->setTemplate('page/1column.phtml');
-      $this->renderLayout();
+
       if(!Mage::helper('blog')->getEnabled()== '2'){
           Mage::getSingleton("core/session")->addError("Some error message");       }
 
@@ -19,9 +17,19 @@ class AW_Blog_MyController extends Mage_Core_Controller_Front_Action
 
         $this->loadLayout();
         echo Mage::helper('blog')->getEnabled();
+        $this->getLayout()->getBlock('root')->setTemplate('page/3columns.phtml');
         $this->renderLayout();
 
 
+    }
+
+    public function finedAction(){
+
+         $this->loadLayout();
+        $this->getLayout()->getBlock('root')->setTemplate('page/1column.phtml');
+        echo Mage::helper('blog')->getEnabled();
+        echo Mage::helper('blog')->isShowcolor();
+         $this->renderLayout();
     }
     //asdfsafd
 
